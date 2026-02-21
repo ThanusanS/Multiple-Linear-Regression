8ab7d2c95493b7580fe31fc683c7fa37aadc1862
ThanusanS <thanusan0411@gmail.com>
2026-02-21 09:38:43 +0530

    Added File

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 import numpy as np
 import pandas as pd
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -105,4 +106,5 @@ def health_check():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
